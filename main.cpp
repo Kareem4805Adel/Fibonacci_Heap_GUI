eb2625bf2f237d953f97d67f3b75601f2fa9cd8b
Kareem4805Adel <107364117+Kareem4805Adel@users.noreply.github.com>
2025-12-22 23:21:17 +0200

    Update project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    cout << "helloWorld";
     return a.exec();
 }
